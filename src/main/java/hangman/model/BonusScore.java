a3e38c1a18e72b0bc103e6abdb65bf71c11400cd
carol695 <caroltatianacely@gmail.com>
2022-09-23 21:26:19 -0500

    Implementacion de metodos

--- src/main/java/hangman/model/BonusScore.java
+++ src/main/java/hangman/model/BonusScore.java
@@ -2,12 +2,13 @@ package hangman.model;
 
 public class BonusScore implements GameScore {
     /**
-     * @pre Inicia con 0 puntos, puntaje minimo 0
+     * @pre Inicia con 0 puntos, puntaje minimo 0 *
      * @pos Se bonifica con 5^numeroLetra, Se penaliza con -8 puntos cada letra incorrecta
      * @param correctCount numero de letras correctas
      * @param incorrectCount numero de letras incorrectas
      * @return Score,  si el score supera 500 puntos, el jugador obtiene como puntaje final 500.
      */
+
     @Override
     public int CalculateScore(int correctCount, int incorrectCount) throws GameScoreException {
         if(correctCount < 0 || incorrectCount < 0) {throw new GameScoreException(GameScoreException.INVALID_PARAMETERS);}
